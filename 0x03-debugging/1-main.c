078dc606f0f6023942cc86a20c8c6cf9ea43fa07
Bintadez <bintadez21@gmail.com>
2023-04-27 15:52:07 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+
+	/*
+	* while (i < 10)
+	*{
+	*	putcha(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
