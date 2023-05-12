a6fcb0d4692718585b77375fda5c78ab517830dd
Bintadez <bintadez21@gmail.com>
2023-05-12 14:05:35 -0700

    temporary change

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - checks if a character c is an uppercase
+ *@c: character to check
+ * Return: 1 if c is an uppercase ,0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
